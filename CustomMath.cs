f5501743af72da026fd99452e4c3633ecb36e145
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2021-03-01 14:22:37 -0600

    Funcion Suma

--- CustomMath.cs
+++ CustomMath.cs
@@ -2,6 +2,9 @@ namespace SimpleMath
 {
     public static class CustomMath
     {
-        
+        public static float Sumar(float numero1, float numero2)
+        {
+            return numero1 + numero2;
+        }
     }
 }
