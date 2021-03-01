f4bac0a756069c3134374fabb60ab2a5508b97ab
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2021-03-01 14:34:20 -0600

    terminando esto

--- CustomMath.cs
+++ CustomMath.cs
@@ -1,7 +1,27 @@
+using System;
+
 namespace SimpleMath
 {
     public static class CustomMath
     {
+
+        public static double Resta() 
+        {
+            double resultado;
+            Console.WriteLine("ingrese primer valor");
+            int valor1 = Console.Read();
+
+            Console.WriteLine("ingrese segundo valor valor");
+            int valor2 = Console.Read();
+
+
+            resultado = valor1 - valor2;
+
+
+
+
+            return resultado;
+        }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace SimpleMath
 {
     class Program
@@ -20,39 +21,41 @@ namespace SimpleMath
                 Console.WriteLine("8. Raíz");
                 string option = Console.ReadLine();
 
-                if(option.Trim() == "1")
+                if (option.Trim() == "1")
+                {
+                    
+                    
+                }
+                else if (option.Trim() == "2")
+                {
+                    Console.WriteLine("el resultado es "+ CustomMath.Resta());
+                   
+                }
+                else if (option.Trim() == "3")
                 {
 
                 }
-                else if(option.Trim() == "2")
+                else if (option.Trim() == "4")
                 {
 
                 }
-                else if(option.Trim() == "3")
+                else if (option.Trim() == "5")
                 {
 
                 }
-                else if(option.Trim() == "4")
+                else if (option.Trim() == "6")
                 {
 
                 }
-                else if(option.Trim() == "5")
+                else if (option.Trim() == "7")
                 {
 
                 }
-                else if(option.Trim() == "6")
+                else if (option.Trim() == "8")
                 {
 
                 }
-                else if(option.Trim() == "7")
-                {
-
-                }
-                else if(option.Trim() == "8")
-                {
-
-                }
-                else 
+                else
                     break;
             }
         }
